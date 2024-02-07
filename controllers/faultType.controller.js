001b66a5408411ed7d511512762c942b99d0c004
asafzaf <asaf.zafrir@gmail.com>
2024-02-07 17:24:35 +0200

    extend functions from router to controller.

---
+++ controllers/faultType.controller.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const {
+  NotFoundError,
+  BadRequestError,
+  ServerError,
+} = require("../errors/errors");
+const { EventEmitter } = require("stream");
+
+const instance = fs.readFileSync("./data/faultCollection.json", "utf-8");
+
+module.exports = class FaultTypeController extends EventEmitter {
+  constructor() {
+    super();
+    this.e = "e";
+  }
+
+  getAllFaultTypes = (req, res) => {
+    try {
+      const resultData = JSON.parse(instance);
+      res.status(200).json({
+        message: "domain retrieved successfully",
+        status: "success",
+        data: {
+          resultData,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof NotFoundError ||
+        error instanceof BadRequestError ||
+        error instanceof ServerError
+      ) {
+        res.status(error.statusCode).json(error.message);
+      } else {
+        res.status(500).send("Internal Server Error");
+      }
+    }
+  };
+  getDomainById = (req, res) => {
+    try {
+      if (!req.params.id) {
+        throw new BadRequestError("id");
+      }
+      const id = req.params.id;
+      const data = JSON.parse(instance);
+      const result = data.domains.filter((item) => item.id === id);
+      if (result.length === 0) throw new NotFoundError("fault type");
+      res.status(200).json({
+        message: "domain retrieved successfully",
+        status: "success",
+        data: {
+          result,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof NotFoundError ||
+        error instanceof BadRequestError ||
+        error instanceof ServerError
+      ) {
+        res.status(error.statusCode).json(error.message);
+      } else {
+        res.status(500).send("Internal Server Error");
+      }
+    }
+  };
+  
+};
--- routers/faultType.router.js
+++ routers/faultType.router.js
@@ -1,40 +1,12 @@
 const { Router } = require("express");
-const fs = require("fs");
-const faultTypeRouter = new Router();
-const {
-  NotFoundError,
-  BadRequestError,
-  ServerError,
-} = require("../errors/errors");
+const faultTypeController = require("../controllers/faultType.controller");
 
-const instance = fs.readFileSync("./data/faultCollection.json", "utf8");
+const faultTypeRouter = new Router();
+const faultTypeControllerInstance = new faultTypeController();
 
 faultTypeRouter
-  .get("/all", (req, res) => {
-    res.json(JSON.parse(instance));
-  })
-  .get("/domain/:id", (req, res) => {
-    try {
-      if (!req.params.id) {
-        throw new BadRequestError("id");
-      }
-      const id = req.params.id;
-      const data = JSON.parse(instance);
-      const result = data.domains.filter((item) => item.id === id);
-      if (result.length === 0) throw new NotFoundError("fault type");
-      res.json(result);
-    } catch (error) {
-      if (
-        error instanceof NotFoundError ||
-        error instanceof BadRequestError ||
-        error instanceof ServerError
-      ) {
-        res.status(error.statusCode).json(error.message);
-      } else {
-        res.status(500).send("Internal Server Error");
-      }
-    }
-  })
+  .get("/all", faultTypeControllerInstance.getAllFaultTypes)
+  .get("/domain/:id", faultTypeControllerInstance.getDomainById)
   .all((req, res, next) => {
     res.status(404).send("Resource not found");
   });
