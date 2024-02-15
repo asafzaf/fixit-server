56648e4621f001f4c96dd2453c00fd4ea0e358d5
asafzaf <asaf.zafrir@gmail.com>
2024-02-15 12:23:47 +0200

    added jwt env params

--- constants.js
+++ constants.js
@@ -5,4 +5,6 @@ module.exports = {
   DB_USER: process.env.DB_USER,
   DB_HOST: process.env.DB_HOST,
   NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 };
