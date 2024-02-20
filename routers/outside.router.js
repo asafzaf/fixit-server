8883e40d8a79e5ea055ebc961b0453f81f42f7e4
asafzaf <asaf.zafrir@gmail.com>
2024-02-20 19:31:35 +0200

    fixed route

--- routers/outside.router.js
+++ routers/outside.router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const outsideController = require('../controllers/outside.controller');
 const router = express.Router();
 
-router.route('/all').get(outsideController.getAllOutsides);
+router.route('/').get(outsideController.getAllOutsides);
 
 module.exports = router;
