fdaa8a20b6cc5ce8ed46f952d13097b3547d4a38
Eliya <eliysamaea@gmail.com>
2024-03-03 20:28:15 +0200

    adding routes for update and delete

--- routers/fault.router.js
+++ routers/fault.router.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.route("/user/:id").get(faultController.getAllFaultsById);
 router.route("/:id").get(faultController.getFaultById);
+router.route("/:id").put(faultController.updateFault);
+router.route("/:id").delete(faultController.deleteFault);
 router
   .route("/")
   .get(faultController.getAllFaults)
-  .post(faultController.uploadFaultPhoto, faultController.createFault)
-  .put(faultController.updateFault);
-  // .delete(faultController.deleteFault);
+  .post(faultController.uploadFaultPhoto, faultController.createFault);
 
 module.exports = router;
