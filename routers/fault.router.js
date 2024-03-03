d955abbdf22eaa7bacb5befc8bed4b328bfe9b49
asafzaf <asaf.zafrir@gmail.com>
2024-03-03 15:54:22 +0200

    added middleware to uplode photo and find by id

--- routers/fault.router.js
+++ routers/fault.router.js
@@ -3,11 +3,11 @@ const faultController = require("../controllers/fault.controller");
 const router = express.Router();
 
 router.route("/user/:id").get(faultController.getAllFaultsById);
-
+router.route("/:id").get(faultController.getFaultById);
 router
   .route("/")
   .get(faultController.getAllFaults)
-  .post(faultController.createFault)
+  .post(faultController.uploadFaultPhoto, faultController.createFault)
   .put(faultController.updateFault);
   // .delete(faultController.deleteFault);
 
