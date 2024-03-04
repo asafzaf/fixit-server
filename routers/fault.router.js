55fb2cd1503f5a420d55972e27fac2c1c516e690
Eliya <eliysamaea@gmail.com>
2024-03-04 17:36:11 +0200

    adding router for getting all faults by building id

--- routers/fault.router.js
+++ routers/fault.router.js
@@ -3,6 +3,7 @@ const faultController = require("../controllers/fault.controller");
 const router = express.Router();
 
 router.route("/user/:id").get(faultController.getAllFaultsById);
+router.route("/building/:id").get(faultController.getAllFaultsByBuildingId);
 router.route("/:id").get(faultController.getFaultById);
 router.route("/:id").put(faultController.updateFault);
 router.route("/:id").delete(faultController.deleteFault);
