fb4bf2894162d77be2375448097c0523ec7b6172
Eliya <eliysamaea@gmail.com>
2024-03-27 15:11:20 +0200

    adding errors to check edge cases

--- tests/building.test.js
+++ tests/building.test.js
@@ -1,11 +1,11 @@
 const request = require("supertest");
 const app = require("../app/app");
 const buildingRepository = require("../repositories/building.repository");
-// const { ServerError } = require("../errors/errors");
+const { NotFoundError, BadRequestError } = require("../errors/errors");
 
 jest.mock("../repositories/building.repository");
 
-// GET all
+// GET all buildings
 describe("GET /api/v1/building/", () => {
   beforeEach(() => jest.clearAllMocks());
 
@@ -14,15 +14,37 @@ describe("GET /api/v1/building/", () => {
     const mockBuildings = [
       {
         id: "1",
-        donorName: "eliya",
-        amount: "200",
-        location: "modiin",
+        buildingName: "Fernik",
+        floorsNumber: 1,
+        floors: [
+          {
+            floorNumber: 1,
+            spaces: [
+              {
+                spaceNumber: "5",
+                spaceName: "office 5",
+                spaceType: "office",
+              },
+            ],
+          },
+        ],
       },
       {
         id: "2",
-        donorName: "mendi",
-        amount: "300",
-        location: "modiin",
+        buildingName: "Mitchell",
+        floorsNumber: 1,
+        floors: [
+          {
+            floorNumber: 1,
+            spaces: [
+              {
+                spaceNumber: "5",
+                spaceName: "office 6",
+                spaceType: "office",
+              },
+            ],
+          },
+        ],
       },
     ];
 
@@ -40,4 +62,96 @@ describe("GET /api/v1/building/", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual(mockBuildingsRes);
   });
+
+  // ERROR - 400 (BadRequestError)
+  //   it("should return 400 when getting a bad request", async () => {
+  //     buildingRepository.find.mockResolvedValue(new BadRequestError("data"));
+
+  //     const res = await request(app).get("/api/v1/");
+  //     expect(res.statusCode).toEqual(200);
+  //   });
+
+  // ERROR - 404 (NotFoundError)
+  it("should return 404 when no buildings are found", async () => {
+    const mockRes = {
+      data: {
+        buildings: {
+          name: "NotFoundError",
+          status: "fail",
+          statusCode: 404,
+        },
+      },
+      status: "success",
+    };
+
+    buildingRepository.find.mockResolvedValue(new NotFoundError("buildings"));
+
+    const res = await request(app).get("/api/v1/building/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(mockRes);
+  });
+});
+
+// GET building
+describe("GET /api/v1/building/:id", () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  // SUCCESS - 200
+  it("should return a building", async () => {
+    const mockBuilding = [
+      {
+        id: "1",
+        buildingName: "Fernik",
+        floorsNumber: 1,
+        floors: [
+          {
+            floorNumber: 1,
+            spaces: [
+              {
+                spaceNumber: "5",
+                spaceName: "office 5",
+                spaceType: "office",
+              },
+            ],
+          },
+        ],
+      },
+    ];
+
+    const mockBuildingsRes = {
+      data: {
+        buildings: mockBuilding,
+      },
+      results: 1,
+      status: "success",
+    };
+
+    buildingRepository.find.mockResolvedValue(mockBuilding);
+
+    const res = await request(app).get("/api/v1/building/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(mockBuildingsRes);
+  });
+
+  // ERROR - 400 (BadRequestError)
+
+  // ERROR - 404 (NotFoundError)
+  //   it("should return 404 when building no found", async () => {
+  //     const mockRes = {
+  //       data: {
+  //         buildings: {
+  //           name: "NotFoundError",
+  //           status: "fail",
+  //           statusCode: 404,
+  //         },
+  //       },
+  //       status: "success",
+  //     };
+
+  //     buildingRepository.find.mockResolvedValue(new NotFoundError("buildings"));
+
+  //     const res = await request(app).get("/api/v1/building/:id");
+  //     expect(res.statusCode).toEqual(200);
+  //     expect(res.body).toEqual(mockRes);
+  //   }, 10000);
 });
